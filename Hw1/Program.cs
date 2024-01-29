29630ff41cf059b41915710fedb506d563c8b935
Alex.LastSurvivour <last.sur.90@gmail.com>
2024-01-29 20:43:49 +0600

    dded folder with some hometaks from the first practical work

---
+++ Hw1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите переменную a с консоли: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите переменную b с консоли: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите переменную c с консоли: ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите переменную c с консоли: ");
+double d = Convert.ToInt32(Console.ReadLine());
+
+
+
+
