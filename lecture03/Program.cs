6cc4170eeadd9832fce1a148623683d455790921
Alex.LastSurvivour <last.sur.90@gmail.com>
2024-02-04 09:40:34 +0600

    Added first task to lecture 3

---
+++ lecture03/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Solve task (a*b)/(c+d) with help of a function
+double CalculateFormula(int a, int b, int c, int d)
+{
+    int nomenator = a * b;
+    double denomenator = c + d;
+    double result = nomenator / denomenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+
+Console.WriteLine(result); 
